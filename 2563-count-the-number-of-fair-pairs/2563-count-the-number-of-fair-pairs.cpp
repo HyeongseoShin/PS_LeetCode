1ff0c743bcdd24843083a73e2e21f7c2bd1522d0
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-14 00:08:34 +0900

    Time: 74 ms (17.26%), Space: 60.3 MB (61.66%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         long long ans = 0;
-        int n = (int)nums.size();
 
-        vector<int> v;
-        v.resize(n);
-
-        for(int i = 0; i < n; i++) v[i] = nums[i];
-
-        sort(v.begin(), v.end());
+        sort(nums.begin(), nums.end());
 
         // 틀렸습니다 원인 나와 Pair될 수는 나 + 1부터 검사
-        for(int i = 0; i < n - 1; i++)
+        for(int i = 0; i < (int)nums.size() - 1; i++)
         {
-            int minVal = lower - v[i];
-            int maxVal = upper - v[i];
+            int minVal = lower - nums[i]; // 더해서 최솟값이 될 수 있는 수
+            int maxVal = upper - nums[i]; // 더해서 최댓값이 될 수 있는 수
 
             // 틀렸습니다 원인 나와 Pair될 수는 나 + 1부터 검사
-            int maxIdx = upper_bound(v.begin() + i + 1, v.end(), maxVal) - v.begin();
-            int minIdx = lower_bound(v.begin() + i + 1, v.end(), minVal) - v.begin();
+            // 최솟값이 될 수 있는 수 ~ 최댓값이 될 수 있는 수 범위에 있는 수는 무조건 Fair함!
+            auto maxIdx = upper_bound(nums.begin() + i + 1, nums.end(), maxVal);
+            auto minIdx = lower_bound(nums.begin() + i + 1, nums.end(), minVal);
 
             ans += (maxIdx - minIdx);            
         }
