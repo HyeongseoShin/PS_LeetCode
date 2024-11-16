2feb47d2abb13ab2e3cc8e0e007c90a2104cacff
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-16 19:13:05 +0900

    Time: 0 ms (100.00%), Space: 33.1 MB (66.21%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,5 +1,6 @@
 /*
-n의 범위가 적어서 이중 for문으로 구현
+n의 범위가 적어서 이중 for문으로 구현했다가
+투포인터 + 슬라이딩 윈도우로 구현
 */
 class Solution {
 public:
@@ -7,25 +8,55 @@ public:
         vector<int> ans;
         int n = (int)nums.size();
 
-        for(int i = 0; i < n - k + 1; i++)
+        // k == 1일때는 원본 배열 return
+        if(k == 1)
         {
-            int maxVal = nums[i];
-            for(int j = 0; j < k - 1; j++)
-            {
-                if(nums[i+j] < nums[i+j+1] && nums[i+j+1] - nums[i+j] < 2)
-                {
-                    maxVal = nums[i+j+1];
-                }
-                else
-                {
-                    maxVal = -1;
-                    break;
-                }
-            }
-            ans.push_back(maxVal);
+            return nums;
         }
 
-        
+        int l = 0;
+        int r = 1;
+
+        while(r < n)
+        {
+            // consecutive, sorted 아니면
+            if(nums[r] - nums[r-1] != 1)
+            {
+                //l ~ r 사이의 시작 원소는 무조건 -1 대입
+                while(l < r && l <= n - k)
+                {
+                    ans.push_back(-1);
+                    l++;
+                }
+                l = r;
+            }
+
+            // l ~ r 개수가 k이면
+            if(r - l + 1 == k)
+            {   ans.push_back(nums[r]);
+                l++;
+            }
+
+            r++;
+        }
+
+        // for(int i = 0; i < n - k + 1; i++)
+        // {
+        //     int maxVal = nums[i];
+        //     for(int j = 0; j < k - 1; j++)
+        //     {
+        //         if(nums[i+j] < nums[i+j+1] && nums[i+j+1] - nums[i+j] < 2)
+        //         {
+        //             maxVal = nums[i+j+1];
+        //         }
+        //         else
+        //         {
+        //             maxVal = -1;
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back(maxVal);
+        // }
 
         return ans;
     }
