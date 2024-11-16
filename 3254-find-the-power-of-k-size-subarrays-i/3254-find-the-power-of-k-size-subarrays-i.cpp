73c3d55787c9b87bf65db463b953afcaa389899c
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-16 18:33:41 +0900

    Time: 7 ms (20.42%), Space: 33.2 MB (55.99%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,32 @@
+/*
+n의 범위가 적어서 이중 for문으로 구현
+*/
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        vector<int> ans;
+        int n = (int)nums.size();
+
+        for(int i = 0; i < n - k + 1; i++)
+        {
+            int maxVal = nums[i];
+            for(int j = 0; j < k - 1; j++)
+            {
+                if(nums[i+j] < nums[i+j+1] && nums[i+j+1] - nums[i+j] < 2)
+                {
+                    maxVal = max(maxVal, nums[i+j+1]);
+                }
+                else
+                {
+                    maxVal = -1;
+                    break;
+                }
+            }
+            ans.push_back(maxVal);
+        }
+
+        
+
+        return ans;
+    }
+};
