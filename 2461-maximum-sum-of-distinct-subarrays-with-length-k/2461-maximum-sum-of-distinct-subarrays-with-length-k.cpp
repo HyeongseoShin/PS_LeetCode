e72a6bdb78d6c8f356689051c72db61b674fa794
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-19 19:56:09 +0900

    Time: 6 ms (97.85%), Space: 67.8 MB (99.63%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,17 +1,23 @@
+/*
+vis를 배열 대신 unordered_map<int, int>형태로 사용하면 불필요한 수에 대한 방문을 초기화할 필요가 없음!
+
+그러나 실행속도가 배열보다 unordered_map이 느렸음
+*/
+
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long ans = 0;
+        
         int vis[100005];
+        fill(vis, vis + 100005, 0);
+
         int n = nums.size();
         int l = 0;
         long long sum = 0;
 
-        fill(vis, vis + 100005, 0);
-
         for(int i = 0; i < n; i++)
         {
-            
             while(vis[nums[i]] != 0)
             {
                 sum -= nums[l];
