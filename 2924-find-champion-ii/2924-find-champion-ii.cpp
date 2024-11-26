4a2ce99de831f6414281112cad8672dce7d08878
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-26 11:09:57 +0900

    Time: 43 ms (41.12%), Space: 104 MB (50.25%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -5,15 +5,12 @@ DFS인줄 알았으나 쓸 필요도 없음
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        int ans = -1;
-        vector<int> cnt;
-        cnt.resize(n);
-
-        fill(cnt.begin(), cnt.end(), 0);
+        vector<int> cnt(n);
 
         // 현재 노드로 들어오는 incoming edge 수 계산
         for(auto edge : edges) cnt[edge[1]]++;
 
+        int ans = -1;
         for(int i = 0; i < n; i++)
         {
             if(cnt[i] == 0)
