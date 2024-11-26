5b61ae869f89679e6cb5331ad3d17cbf0e0a957a
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-11-26 11:08:35 +0900

    Time: 69 ms (9.81%), Space: 104.2 MB (38.58%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,39 +1,29 @@
 /*
-DFS
+DFS인줄 알았으나 쓸 필요도 없음
+나로 들어오는 incoming edge가 0이고 유일한 노드일 때 (중복 X)
 */
 class Solution {
 public:
-    bool vis[200];
-    vector<int> nums[200];
-    int ans = -1;
-
-    void DFS(int x)
-    {
-        ans++;
-        for(auto nxt : nums[x])
-        {
-            if(vis[nxt]) continue;
-            vis[nxt] = true;
-            DFS(nxt);
-        }
-        
-    }
     int findChampion(int n, vector<vector<int>>& edges) {
-        for(int i = 0; i < (int)edges.size(); i++)
-        {
-            nums[edges[i][0]].push_back(edges[i][1]);
-        }
+        int ans = -1;
+        vector<int> cnt;
+        cnt.resize(n);
+
+        fill(cnt.begin(), cnt.end(), 0);
+
+        // 현재 노드로 들어오는 incoming edge 수 계산
+        for(auto edge : edges) cnt[edge[1]]++;
 
         for(int i = 0; i < n; i++)
         {
-            ans = -1;
-            fill(vis, vis + n, false);
-            vis[i] = true;
-            DFS(i);
-
-            if(ans == n - 1) return i;
+            if(cnt[i] == 0)
+            {
+                // 챔피언이 여러명일 수 없음
+                if(ans != -1) return -1;
+                ans = i;
+            }
         }
 
-        return -1;
+        return ans;
     }
 };
