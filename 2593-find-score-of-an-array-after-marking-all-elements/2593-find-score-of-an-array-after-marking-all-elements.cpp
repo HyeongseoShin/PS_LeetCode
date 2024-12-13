8a78091771ad0fe2706c6a444d3960753b466461
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-12-13 23:42:28 +0900

    Time: 55 ms (93.86%), Space: 105.6 MB (57.19%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    struct cmp
-    {
-        bool operator()(pair<int, int> A, pair<int, int> B)
-        {
-            if(A.first != B.first)
-            {
-                return A.first > B.first;
-            }
-            else
-            {
-                return A.second > B.second;
-            }
-        }
-    };
-    
     long long findScore(vector<int>& nums) {
         long long sum = 0;
         int n = (int)nums.size();
+        
+        vector<pair<int, int>> v;
         vector<bool> vis (n, false);
-
-        priority_queue<pair<int, int>, vector<pair<int,int>>, cmp> pq;
 
         for(int i = 0; i < n; i++)
         {
-            pq.push({nums[i], i});
+            v.push_back({nums[i], i});
         }
 
-        while(!pq.empty())
+        sort(v.begin(), v.end());
+
+        for(int i = 0; i < n; i++)
         {
-            int val = pq.top().first;
-            int idx = pq.top().second;
+            int val = v[i].first;
+            int idx = v[i].second;
 
-            pq.pop();
-
-            if(vis[idx])
-            {
-                continue;
-            }
+            if(vis[idx]) continue;
 
             vis[idx] = true;
-
-            if(idx < n - 1) vis[idx + 1] = true;
             if(idx > 0) vis[idx - 1] = true;
+            if(idx < n - 1) vis[idx + 1] = true;
 
             sum += val;
         }
