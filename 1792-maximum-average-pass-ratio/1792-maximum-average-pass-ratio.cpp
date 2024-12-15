97e36be9228d726705da8afaf3767c3168e8283c
HyeongseoShin <105703629+HyeongseoShin@users.noreply.github.com>
2024-12-15 22:44:45 +0900

    Time: 326 ms (70.09%), Space: 99.1 MB (39.10%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,15 +1,23 @@
+/*
+우선순위 큐 이용
+*/
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         double ans = 0;
+
+        // <extraStudents가 투입되었을 때의 평균 - 현재 평균, 현재 pass, 현재 total>
         priority_queue<tuple<double, int, int>, vector<tuple<double, int, int>>> pq;
 
         int n = (int)classes.size();
-
+        
+        // extraStudents가 투입되었을 때 평균 상승량이 가장 큰 학급에 extraStudents 투입
         for(int i = 0; i < n; i++)
         {
             double newPr = (double)(classes[i][0] + 1) / (classes[i][1] + 1);
             double curPr = (double)classes[i][0] / classes[i][1];
+
+            ans += curPr;
             pq.push({newPr - curPr, classes[i][0], classes[i][1]});
         }
 
@@ -18,30 +26,14 @@ public:
             auto [pr, p, t] = pq.top();
             pq.pop();
 
-            // cout << "pr: " << pr << " p: " << p << " t: " << t << "\n";
-
             double newPr = (double)(p+2) / (t+2);
             double curPr = (double)(p+1) / (t+1);
-
+            
+            ans -= (double)p / t;
+            ans += curPr;
+            
             pq.push({newPr - curPr, p+1, t+1});
         }
-
-        while(!pq.empty())
-        {
-            auto [pr, p, t] = pq.top();
-            pq.pop();
-
-            // cout << "p: " << p << " t: " << t << "\n";
-            // cout << "res: " << (double)p / t << "\n";
-
-            ans += (double)p / (double)t;
-
-            // cout << "ans: " << ans << "\n";
-
-            
-        }
-
-        
 
         return (double)ans / n;
     }
